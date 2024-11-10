a6e87dc813bc21791ad3ab4a814ac961fc062a4a
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2024-11-10 19:23:33 +0300

    java subarray solved

---
+++ Java Hackerrank/JavaSubarray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JavaSubarray {
+    public static void main( String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = int[n];
+        for(int a=0; a<n ; a++){
+            arr[a] = scanner.nextInt();
+        }
+        int negativeSum = 0;
+        for(int i=0; i<n ; i++){
+            int sum = 0;
+            for(int k=i; k<n; k++){
+                sum += arr[k];
+                if(sum < 0) negativeSum ++;
+            }
+        }
+    }
+}
